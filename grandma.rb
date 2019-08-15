ea010d06143554c90183762c1257c11f100020ae
Ryan S <planetlucid@gmail.com>
2019-08-15 01:47:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,14 @@
 # Write a speak_to_grandma method.
-
+if phrase = "lowercase"
+  puts "HUH?! SPEAK UP, SONNY!"
+  
+else phrase ="UPPERCASE YELLING"
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+
+speak_to_grandma 
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
